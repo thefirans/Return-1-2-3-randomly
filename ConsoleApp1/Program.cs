1f4e7347e5c73a2fb1199d801fea20d0885bddf2
Sasha <sleo170504@gmail.com>
2022-10-19 13:15:46 +0300

    Solution

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Program1
+    {
+        public static int OneTwoThree()
+        {
+            var random = new System.Random();
+            return random.Next() * (3 - 1) + 1;
+        }
+    }
+}
